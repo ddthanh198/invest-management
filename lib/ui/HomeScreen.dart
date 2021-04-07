c9dc16b002bac17af687443b3215caf44c714435
thanhdd21 <Jack02@@>
2021-04-07 19:13:17 +0700

    card view

--- lib/ui/HomeScreen.dart
+++ lib/ui/HomeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,29 +19,83 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(color: Colors.white),
         ),
         actions: [
-          IconButton(
-            icon: Image.asset(
-              'assets/icons/ic_reload.png',
-              color: Colors.white,
+          Container(
+            padding: const EdgeInsets.all(0),
+            width: 36,
+            child: IconButton(
+              icon: Image.asset(
+                'assets/icons/ic_reload.png',
+                color: Colors.white,
 
+              ),
+              onPressed: () {
+
+              }
             ),
-            onPressed: () {
-
-            }
           ),
-          IconButton(
-            icon: Image.asset(
-              'assets/icons/ic_add_asset.png',
-              color: Colors.white,
+          Container(
+            margin: const EdgeInsets.fromLTRB(5, 0, 10, 0),
+            padding: const EdgeInsets.all(0),
+            width: 36,
+            child: IconButton(
+              icon: Image.asset(
+                'assets/icons/ic_add_asset.png',
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("press add");
+              }
             ),
-            onPressed: () {
-
-            }
           )
         ],
       ),
       body: Container(
+        padding: const EdgeInsets.all(5),
         color: HexColor("#F2F5FA"),
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            "Tổng nguồn vốn",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                        ),
+                        Text(
+                            "1000.000.000 vnd",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black
+                          ),
+                        ),
+                        Text(
+                            "-20.000.000 (-50 %)",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SfCircularChart(
+
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
