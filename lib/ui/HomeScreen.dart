d4b7349b82b883cf28905494ceba5fa6337128ff
Jack Dao <ddthanh198.develop@gmail.com>
2021-04-07 23:38:11 +0700

    chart

--- lib/ui/HomeScreen.dart
+++ lib/ui/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:invest_management/model/PieData.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
@@ -13,10 +14,12 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text(
+        title: const Text(
           "Danh mục đầu tư",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.black),
         ),
         actions: [
           Container(
@@ -25,7 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: IconButton(
               icon: Image.asset(
                 'assets/icons/ic_reload.png',
-                color: Colors.white,
+                color: Colors.black,
 
               ),
               onPressed: () {
@@ -40,7 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: IconButton(
               icon: Image.asset(
                 'assets/icons/ic_add_asset.png',
-                color: Colors.white,
+                color: Colors.black,
               ),
               onPressed: () {
                 print("press add");
@@ -53,6 +56,9 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(5),
         color: HexColor("#F2F5FA"),
         child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -64,32 +70,59 @@ class _HomeScreenState extends State<HomeScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                            "Tổng nguồn vốn",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                        ),
-                        Text(
-                            "1000.000.000 vnd",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black
+                        Container(
+                          margin: EdgeInsets.all(2),
+                          child: const Text(
+                              "Tổng nguồn vốn",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
                           ),
                         ),
-                        Text(
-                            "-20.000.000 (-50 %)",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.red
+                        Container(
+                          margin: EdgeInsets.all(2),
+                          child: Text(
+                              "1000.000.000 vnd",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(2),
+                          child: Text(
+                              "-20.000.000 (-50 %)",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.red
+                            ),
                           ),
                         ),
                       ],
                     ),
                   ),
-                  SfCircularChart(
-
+                  SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: SfCircularChart(
+                        series: <DoughnutSeries<PieData, String>>[
+                          DoughnutSeries<PieData, String>(
+                              innerRadius: "50%",
+                              dataSource: [
+                                PieData("hello", 10, Colors.yellow ,"10%"),
+                                PieData("hello", 20, Colors.red,"20%"),
+                                PieData("hello", 30, Colors.blue,"30%"),
+                                PieData("hello", 40, Colors.green,"40%"),
+                              ],
+                              pointColorMapper: (PieData data, _) => data.color,
+                              xValueMapper: (PieData data, _) => data.xData,
+                              yValueMapper: (PieData data, _) => data.yData,
+                              dataLabelMapper: (PieData data, _) => data.text,
+                              dataLabelSettings: DataLabelSettings(isVisible: true)),
+                        ]
+                    ),
                   )
                 ],
               )
